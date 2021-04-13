35d74dfb8ab7036d80ce56092ace8bf234da4ec8
yoyrandao@gmail.com <yoyrandao@gmail.com>
2021-04-13 21:48:48 +0300

    configured profiles and setup script

--- setup.sh
+++ setup.sh
@@ -21,13 +21,16 @@ echo 'executing docker-compose. initializing...';
 docker network create registry_default;
 docker network create portainer_default;
 
-if [ $1 = '-withRegistry' ]
+if [ $# != 0 ]
 then
-  docker-compose -f install/docker-compose.yml --env-file install/.env --profile withRegistry up -d;
-  echo 'droplet initialized.';
-  exit 0;
+  if [ $1 = '-withRegistry' ]
+  then
+    docker-compose -f install/setup.full.yml --env-file install/.env up -d;
+    echo 'droplet initialized.';
+    exit 0;
+  fi
 fi
 
-docker-compose -f install/docker-compose.yml --env-file install/.env up -d;
+docker-compose -f install/setup.yml --env-file install/.env up -d;
 echo 'droplet initialized.';
 exit 0;
